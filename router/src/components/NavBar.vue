7fd53e115e36537371f7e1000510b8710d57b053
scorpion290 <darkshadic99@gmail.com>
2022-03-02 13:03:15 -0500

    ddd

---
+++ router/src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+<nav>
+    <h1>LEAGUE OF LEGENDS</h1>
+    <div>
+        <router-link to="/">Home</router-link>
+    </div>
+    <div>
+        <span>Logged in as...</span>
+        <button>Logout</button>
+    </div>
+    <div>
+        <router-link to="/login">Login</router-link>
+        <router-link to="signup">Signup</router-link>
+    </div>
+</nav>
+</template>
