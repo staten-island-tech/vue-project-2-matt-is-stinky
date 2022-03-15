f80787699dc5d5653168675a6df93a9dbdb41d98
scorpion290 <darkshadic99@gmail.com>
2022-03-15 13:11:41 -0400

    tried to create database upload

--- LOL/src/views/CreateCard.vue
+++ LOL/src/views/CreateCard.vue
@@ -1,4 +1,20 @@
 <template>
+  <form>
+  <div>
+  <label for="name">Name: </label>
+  <input required type="text" id="name" v-model="name" />
+  </div>
+  <!--- <div>
+    <input type="file" @change="onFileSelected" refname="image">
+  </div> --->
+  <div>
+  <label for="description">Description: </label>
+  <input required type="text" id="description" v-model="description" />
+  </div>
+  <button @click="addNewCard" class="button">
+    Create Card!
+  </button>
+  </form>
 </template>
 
 <script>
@@ -7,8 +23,21 @@ export default {
   data() {
     return {
       name: null,
-      image: null,
+      /*selectedFile: null,*/
       description: null,
+    }
+  },
+  methods: {
+    /*onFileSelected(event) {
+      this.selectedFile - event.target.files[0]
+    },
+    onUpload() {
+
+    }*/
+    addNewCard() {
+    },
+    async uploadCard() {
+      
     }
   }
 }
