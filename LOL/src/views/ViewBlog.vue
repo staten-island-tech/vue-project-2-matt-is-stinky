310ea3b9167f7db011640497c07e839b0b4691f5
EdwinZ11 <90789780+EdwinZ11@users.noreply.github.com>
2022-05-05 13:11:17 -0400

    start view blog thingy

---
+++ LOL/src/views/ViewBlog.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="post-view" v-if="currentBlog">
+        <div>
+            <h2>{{ this.currentBlog[0].blogTitle }}</h2>
+            <h3> Posted On:{{  }}</h3>
+        </div>
+    </div>
+</template>
+<script>
+
+</script>
