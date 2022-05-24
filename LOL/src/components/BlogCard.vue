cf1c990921f683429d2ddc18b2ef5f340d589927
EdwinZ11 <90789780+EdwinZ11@users.noreply.github.com>
2022-05-24 13:11:20 -0400

    rounding + color change

--- LOL/src/components/BlogCard.vue
+++ LOL/src/components/BlogCard.vue
@@ -6,9 +6,9 @@
       </div>
     </div>
     <router-link
-:to="{ name: 'View Blog', params: { postID: this.post.postID } }"
+      :to="{ name: 'View Blog', params: { postID: this.post.postID } }"
     >
-      <img class="image" :src="post.postPhoto" alt="" />
+      <img :src="post.postPhoto" alt="" />
     </router-link>
   </div>
 </template>
@@ -31,17 +31,67 @@ export default {
 </script>
 
 <style scoped>
-.blog-card{
+.blog-card {
+  position: relative;
+  cursor: pointer;
   display: flex;
-  object-fit: cover;
+  flex-direction: column;
+  min-height: 8rem;
   transition: 0.5s ease all;
 }
 
 .blog-card:hover {
   transform: rotateZ(-1deg) scale(1.01);
-  background-color: #000;
   box-shadow: 0 0.4rem 0.6rem -0.1rem rgba(0, 0, 0, 0.1),
     0 0.2rem 0.4rem -0.1rem rgba(0, 0, 0, 0.06);
+}
+
+img {
+  display: block;
+  z-index: 1;
+  width: 100%;
+  border-radius: 2rem;
+}
+
+.info {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  z-index: 3;
+  padding: 3.2rem 1.6rem;
+  color: #000;
+  visibility: hidden;
+}
+
+.info:hover {
+  visibility: visible;
+}
+
+h4 {
+  padding-bottom: 0.8rem;
+  font-size: 2rem;
+  font-weight: 300;
+}
+
+h6 {
+  font-weight: 400;
+  font-size: 1.2rem;
+  padding-bottom: 1.6rem;
+}
+
+.link {
+  display: inline-flex;
+  align-items: center;
+  margin-top: auto;
+  font-weight: 500;
+  padding-top: 2rem;
+  font-size: 1.2rem;
+  padding-bottom: 0.4rem;
+  transition: 0.5s ease-in all;
+}
+
+.link:hover {
+  color: rgba(48, 48, 48, 0.8);
 }
 
 .editButtons {
